39d226ed3dde8a03e4458ff104bba69071d73051
Paul Hine <paul.hine@gmail.com>
2025-03-15 03:57:22 +0800

    message queuing and throttling

--- electron/electron/main/serial.ts
+++ electron/electron/main/serial.ts
@@ -1,7 +1,15 @@
 import { SerialPort, ReadlineParser } from 'serialport'
 
+const MAX_RETRIES = 4
+const MESSAGE_INTERVAL = 300
+
 let port: SerialPort = null
 let parser: ReadlineParser = null
+const messageQueue: string[] = []
+let retryCount = 0
+let inflightMessage = null
+
+setInterval(processMessageQueue, MESSAGE_INTERVAL)
 
 export function startSerialComms(ipcMain: Electron.IpcMain) {
   port = new SerialPort({ 
@@ -20,7 +28,7 @@ export function startSerialComms(ipcMain: Electron.IpcMain) {
   
   // Read the port data
   parser.on('data', data =>{
-    receiveMessage(data)
+    receive(data)
   });
 
   ipcMain.handle('serial-test', (_, arg) => {
@@ -28,11 +36,35 @@ export function startSerialComms(ipcMain: Electron.IpcMain) {
   })
 }
 
-function receiveMessage(msg: string) {
+function receive(msg: string) {
   console.log('[serial] rx', msg)
+  if (!msg.startsWith('ACK')) {
+    return
+  }
+  const ackMessage = msg.substring(4).trim()
+  const expectedMessage = inflightMessage.trim()
+
+  // clear inflight message so we can start processing the queue again
+  inflightMessage = null
+
+  if (ackMessage == expectedMessage) {
+    // success, reset retry count
+    retryCount = 0
+    console.log('[serial] sent', messageQueue)
+    return
+  }
+  // fail, so bump retryCount
+  retryCount += 1
+  if (retryCount > MAX_RETRIES) {
+    // reset the count so we can move on
+    retryCount = 0
+    return
+  }
+  // re-queue the message so we try again
+  messageQueue.unshift(expectedMessage)
 }
 
-export function sendMessage(msg: string): boolean {
+function transmit(msg: string): boolean {
   if (!port) {
     throw(new Error('Serial port not open'))
   }
@@ -48,4 +80,35 @@ export function sendMessage(msg: string): boolean {
   })
 
   return true
+}
+
+function processMessageQueue() {
+  // empty q? do nothing
+  if (!messageQueue.length) {
+    return
+  }
+  // message in flight? need to wait for ACK
+  if (inflightMessage) {
+    return
+  }
+  const msg = messageQueue.shift()
+  if (!transmit(msg)) {
+    // if transmit fails, re-queue the message
+    retryCount += 1
+    if (retryCount > MAX_RETRIES) {
+      // reset the count, warn, and move on
+      retryCount = 0
+      console.warn('[serial] message transmit exceeded retry count, skipping', msg)
+      return
+    }
+    messageQueue.unshift(msg)
+    return
+  }
+  // transmit succeeded, set inflight msg and wait for ACK
+  inflightMessage = msg
+}
+
+export function sendMessage(msg: string) {
+  messageQueue.push(msg)
+  console.log('[serial] message queue', messageQueue)
 }
